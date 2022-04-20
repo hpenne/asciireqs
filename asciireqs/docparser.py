5821c4050b2aea76c11584c43a2a08b3dcfa55a0
Helge Penne <hpenne@gmail.com>
2022-04-20 06:28:26 +0200

    Removed table-based requirements from the code.

--- asciireqs/docparser.py
+++ asciireqs/docparser.py
@@ -34,118 +34,9 @@ class Location:
     line: int
 
 
-@dataclass
-class Cell:
-    """This class holds the contents of a table cell"""
-
-    data: str
-    location: Location
-
-    def empty(self) -> bool:
-        """Returns True if the Cell is empty"""
-        return not self.data
-
-
-def empty_cell() -> Cell:
-    """Returns an empty Cell"""
-    return Cell("", Location(0))
-
-
-Cells = List[Cell]
-Row = Cells
-Table = List[Row]
-
-
-def append_cells(table_rows: Table, num_columns: int, cells: Cells) -> None:
-    """Takes a list of cells and appends them to the table, breaking them into rows"""
-    if not table_rows and len(cells) > 0:
-        table_rows.append(cells)
-    else:
-        for cell in cells:
-            if len(table_rows[-1]) == num_columns:
-                table_rows.append([cell])
-            else:
-                table_rows[-1].append(cell)
-
-
-def get_cols_from_attribute(line: str, line_no: int) -> Optional[int]:
-    """Interprets an AsciiDoc 'cols' attribute to get the number of table columns"""
-    # This is crude, but it usually works:
-    num_widths = len(line.split(","))
-    if num_widths >= 2:
-        return num_widths
-    eq_pos = line.find("=")
-    bracket_pos = line.find("]")
-    if 0 <= eq_pos < bracket_pos:
-        num_str = line[eq_pos + 1 : bracket_pos]
-        try:
-            return int(num_str)
-        except ValueError:
-            pass
-    print(f"Error on line {line_no}, failed to parse number of columns: {line}")
-    return None
-
-
-def cells_from_line(line: str, line_no: int) -> Cells:
-    """Takes a line of table text and converts it into cells"""
-    cells: Cells = [Cell(cell.strip(), Location(line_no)) for cell in line.split("|")]
-    column_merge = re.compile(r"(\d+)\+")
-    matches = column_merge.search(cells[0].data) if cells[0] else None
-    if matches:
-        # The line starts with a cell merge specifier,
-        # so generate None cells for the unused ones:
-        additional_cells = int(matches.groups()[0]) - 1
-        cells = cells + additional_cells * [empty_cell()]
-    # Drop the "cell" before the vertical bar (not a cell):
-    return cells[1:]
-
-
-def get_table(
-    lines: Iterable[Tuple[int, str]]
-) -> Tuple[Optional[Row], Optional[Table]]:
-    """Takes AsciiDoc lines of text and interprets it to get a Table"""
-    in_table: bool = False
-    num_columns: Optional[int] = None
-    table_rows: Table = []
-    heading_cells: Optional[Row] = None
-    attributes = re.compile(r"\[\w+=.+]")
-    for line_no, line in lines:
-        if in_table:
-            if line.rstrip() == "|===":
-                if len(table_rows[0]) != len(table_rows[-1]):
-                    print(
-                        f"Error on line {line_no}, table missing cell(s) on last row: {line}"
-                    )
-                    return None, None
-                return heading_cells, table_rows
-            if not line.strip():
-                if len(table_rows) == 1 and not heading_cells:
-                    # The first line of cells was the heading:
-                    heading_cells = table_rows[0]
-                    table_rows = []
-                continue
-            cells = cells_from_line(line, line_no)
-            if not num_columns and len(cells) > 0:
-                # If the number of columns has not been set, then this must be the heading row:
-                num_columns = len(cells)
-            if num_columns:
-                append_cells(table_rows, num_columns, cells)
-        else:
-            if line.startswith("[cols"):
-                num_columns = get_cols_from_attribute(line, line_no)
-            if line.rstrip() == "|===":
-                in_table = True
-            elif not attributes.match(line):
-                print(
-                    f"Error on line {line_no}: Expected attributes or table start, but was: {line}"
-                )
-                return None, None
-    return None, None
-
-
 def get_source_block(lines: Iterable[Tuple[int, str]]) -> Tuple[List[str], int]:
     """
-    Takes AsciiDoc lines of text (starting with a source block) and consumes the sopurce block
+    Takes AsciiDoc lines of text (starting with a source block) and consumes the source block
     lines and returns them (not including the '----' start and end lines)
     :param lines: The input lines
     :return: The contents of the source block
@@ -184,55 +75,6 @@ def validate_requirement(req: Requirement, doc: ReqDocument, line_no: int) -> bo
         print(f"Error: Wrong ID format on line {line_no}")
         return False
     return True
-
-
-def reqs_from_req_table(
-    heading: Row, table_rows: Table, doc: ReqDocument
-) -> Iterable[Requirement]:
-    """Takes a table and returns the requirements in it"""
-    if table_rows:
-        for row in table_rows:
-            req = {heading[i].data: cell.data for (i, cell) in enumerate(row)}
-            line_no = row[0].location.line
-            req[LINE_NO] = str(line_no)
-            if validate_requirement(req, doc, line_no):
-                yield req
-
-
-def req_from_single_req_table(
-    table_lines: Table, doc: ReqDocument
-) -> Optional[Requirement]:
-    """Takes a table form and returns the single requirement in it"""
-    # First cell should be requirement ID
-    line_no = table_lines[0][0].location.line
-    req = {
-        ID: table_lines[0][0].data,
-        LINE_NO: str(line_no),
-    }
-    for cell in sum(table_lines, [])[1:]:
-        if cell:
-            parts = [part.strip() for part in cell.data.split(":")]
-            if len(parts) == 1:
-                if TEXT in req:
-                    if not cell.empty():
-                        print(
-                            (
-                                "Error in single req. table: Second non-property/value pair found"
-                                f"(only one allowed): {cell}"
-                            )
-                        )
-                        return None
-                else:
-                    req[TEXT] = parts[0]
-            else:
-                if parts[0]:
-                    req[parts[0]] = parts[1]
-                else:
-                    print(
-                        f"Error in single req. table: Property name not found: {cell}"
-                    )
-                    return None
-    return req if validate_requirement(req, doc, line_no) else None
 
 
 def req_from_yaml_lines(
@@ -280,6 +122,7 @@ def req_from_yaml_block(
     Takes AsciiDoc lines of text (starting with a source block), consumes the source block
     lines, converts to YAML and returns the requirements defined by the YAML
     :param lines: The input lines
+    :param doc: The current document
     :return: The requirements
     """
     yaml_lines, start_line_no = get_source_block(lines)
@@ -294,23 +137,6 @@ def get_attribute(line: str, name: str) -> Optional[str]:
     if line.startswith(attribute):
         return line[len(attribute) :].strip()
     return None
-
-
-def heading_names(heading: Row) -> List[str]:
-    """Takes a list of heading cells and returns the heading names"""
-    return [cell.data for cell in heading]
-
-
-def heading_has_required_fields(heading: Row, line_no: int) -> bool:
-    """Takes a list of heading cells and checks it contains columns for ID and Text"""
-    names = heading_names(heading)
-    if ID not in names:
-        print(f"Error: Table must contain a column named '{ID}' on line {line_no}")
-        return False
-    if TEXT not in names:
-        print(f"Error: Table must contain a column named '{TEXT}' on line {line_no}")
-        return False
-    return True
 
 
 def parse_term_req_attributes(line: str) -> Optional[Dict[str, str]]:
@@ -385,14 +211,6 @@ def parse_doc(lines: Iterable[Tuple[int, str]]) -> ReqDocument:
         term_req = req_from_term(text, line_no, lines, doc)
         if term_req:
             doc.add_req(term_req)
-        elif text == "[.reqs]":
-            heading, rows = get_table(lines)
-            if heading and rows and heading_has_required_fields(heading, line_no):
-                doc.add_reqs(reqs_from_req_table(heading, rows, doc))
-        elif text == "[.req]":
-            heading, rows = get_table(lines)
-            if rows:
-                doc.add_req(req_from_single_req_table(rows, doc))
         elif text == "[.reqy]":
             for req in req_from_yaml_block(lines, doc):
                 doc.add_req(req)
--- tests/test_docparser.py
+++ tests/test_docparser.py
@@ -1,25 +1,15 @@
 """test_docparser: Tests for the docparser modele"""
 
-from typing import List, Tuple
+from typing import Tuple
 from asciireqs.docparser import (
-    Cell,
-    Location,
-    get_table,
-    reqs_from_req_table,
-    req_from_single_req_table,
     get_source_block,
     req_from_yaml_lines,
     req_from_yaml_block,
-    get_cols_from_attribute,
     validate_requirement,
     req_from_term,
 )
 from asciireqs.fields import ID, TEXT, PARENT, CHILD, LINE_NO
 from asciireqs.reqdocument import ReqDocument
-
-
-def row(elements: List[Tuple[str, int]]) -> List[Cell]:
-    return [Cell(value, Location(line)) for value, line in elements]
 
 
 def empty() -> Tuple[str, int]:
@@ -30,96 +20,6 @@ def doc_with_req_prefix() -> ReqDocument:
     doc = ReqDocument()
     doc.req_prefix = "SR-"
     return doc
-
-
-def test_table() -> None:
-    lines = enumerate(['[cols="1,1,1"]', "|===", "| A | B | C", "|==="], start=1)
-    heading, t = get_table(lines)
-    assert t
-    assert len(t) == 1
-    assert t[0] == row([("A", 3), ("B", 3), ("C", 3)])
-
-
-def test_table_single_element_lines() -> None:
-    lines = enumerate(
-        ['[cols="1,1,1"]', "|===", "| A | B | C", "| D", "|E", "| F", "|==="], start=1
-    )
-    heading, rows = get_table(lines)
-    assert not heading
-    assert rows
-    assert len(rows) == 2
-    assert rows[0] == row([("A", 3), ("B", 3), ("C", 3)])
-    assert rows[1] == row([("D", 4), ("E", 5), ("F", 6)])
-
-
-def test_table_with_heading() -> None:
-    lines = enumerate(
-        ["|===", "| 1 | 2 | 3", "", "| A | B | C", "| D", "|E", "| F", "|==="], start=1
-    )
-    heading, rows = get_table(lines)
-    assert heading == [
-        Cell("1", Location(2)),
-        Cell("2", Location(2)),
-        Cell("3", Location(2)),
-    ]
-    assert rows
-    assert len(rows) == 2
-    assert rows[0] == row([("A", 4), ("B", 4), ("C", 4)])
-    assert rows[1] == row([("D", 5), ("E", 6), ("F", 7)])
-
-
-def test_table_missing_element() -> None:
-    lines = enumerate(
-        ['[cols="1,1,1"]', "|===", "| A | B | C", "| D", "|E", "|==="], start=1
-    )
-    heading, rows = get_table(lines)
-    assert not heading
-    assert not rows
-
-
-def test_table_with_incorrect_start() -> None:
-    lines = enumerate(
-        [
-            '[cols="1,1,1"]',
-            "This line does not belong here",
-            "|===",
-            "| A | B | C",
-            "| D | E | F",
-            "|===",
-        ],
-        start=1,
-    )
-    heading, rows = get_table(lines)
-    assert not heading
-    assert not rows
-
-
-def test_single_req_table_with_column_widths() -> None:
-    lines = enumerate(
-        ['[cols="1,1,1"]', "|===", "| A", "| B", "| C", "3+| Merged", "|==="], start=1
-    )
-    heading, rows = get_table(lines)
-    assert not heading
-    assert rows
-    assert len(rows) == 2
-    assert rows[0] == row([("A", 3), ("B", 4), ("C", 5)])
-    assert rows[1] == row([("Merged", 6), empty(), empty()])
-
-
-def test_single_req_table_with_column_count() -> None:
-    lines = enumerate(
-        ["[cols=3]", "|===", "| A", "| B", "| C", "3+| Merged", "|==="], start=1
-    )
-    heading, rows = get_table(lines)
-    assert not heading
-    assert rows
-    assert len(rows) == 2
-    assert rows[0] == row([("A", 3), ("B", 4), ("C", 5)])
-    assert rows[1] == row([("Merged", 6), empty(), empty()])
-
-
-def test_invalid_cols_attribute() -> None:
-    assert not get_cols_from_attribute("[cols=]", 2)
 
 
 def test_validate_requirement() -> None:
@@ -149,72 +49,6 @@ def test_validate_requirement_no_text_field() -> None:
     doc = ReqDocument()
     doc.req_prefix = "SR-"
     assert not validate_requirement({ID: "SR-1"}, doc, 2)
-
-
-def test_reqs_from_reqtable() -> None:
-    heading = row([(ID, 2), (TEXT, 2), ("A", 2)])
-    rows = [
-        row([("SR-1", 3), ("B", 3), ("C", 3)]),
-        row([("SR-2", 4), ("E", 4), ("F", 4)]),
-    ]
-    reqs = list(reqs_from_req_table(heading, rows, doc_with_req_prefix()))
-    assert reqs
-    assert len(reqs) == 2
-    assert reqs[0] == {ID: "SR-1", TEXT: "B", "A": "C", LINE_NO: "3"}
-    assert reqs[1] == {ID: "SR-2", TEXT: "E", "A": "F", LINE_NO: "4"}
-
-
-def test_req_from_single_req_table() -> None:
-    rows = [
-        row([("SR-1", 3), ("Parent: ID-2", 3), ("Child: ID-3", 3)]),
-        row([("Text", 4), empty(), empty()]),
-    ]
-    reqs = req_from_single_req_table(rows, doc_with_req_prefix())
-    assert reqs
-    assert len(reqs) == 5
-    assert reqs == {
-        ID: "SR-1",
-        PARENT: "ID-2",
-        CHILD: "ID-3",
-        TEXT: "Text",
-        LINE_NO: "3",
-    }
-
-
-def test_req_from_single_req_table_with_three_rows() -> None:
-    rows = [
-        row([("SR-1", 3), ("Parent: ID-2", 3)]),
-        row([("Tags: V.1", 3), ("Child: ID-3", 3)]),
-        row([("Text", 4), empty(), empty()]),
-    ]
-    reqs = req_from_single_req_table(rows, doc_with_req_prefix())
-    assert reqs
-    assert len(reqs) == 6
-    assert reqs == {
-        ID: "SR-1",
-        PARENT: "ID-2",
-        CHILD: "ID-3",
-        "Tags": "V.1",
-        TEXT: "Text",
-        LINE_NO: "3",
-    }
-
-
-def test_req_from_single_req_table_with_multiple_text_fields() -> None:
-    rows = [
-        row([("SR-1", 3), ("Parent: ID-2", 3), ("Child: ID-3", 3)]),
-        row([("Text1", 4), empty(), empty()]),
-        row([("Text2", 4), empty(), empty()]),
-    ]
-    assert not req_from_single_req_table(rows, doc_with_req_prefix())
-
-
-def test_req_from_single_req_table_with_missing_attribute_name() -> None:
-    rows = [
-        row([("SR-1", 3), ("Parent: ID-2", 3), (": ID-3", 3)]),
-        row([("Text", 4), empty(), empty()]),
-    ]
-    assert not req_from_single_req_table(rows, doc_with_req_prefix())
 
 
 def test_get_source_block_with_empty_input() -> None:
