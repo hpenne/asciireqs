aff1ba1548697314872c95a530f0bbc040d65ec3
Helge Penne <hpenne@gmail.com>
2024-08-18 21:08:15 +0200

    Bumped version to 0.0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "long-description.md").read_text(encoding="utf-8")
 
 setup(
     name="asciireqs",
-    version="0.0.5",
+    version="0.0.6",
     description="Text-based Requirement Management using AsciiDoc and version control",
     long_description=long_description,
     long_description_content_type="text/markdown",
